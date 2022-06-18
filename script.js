86c94fce98f00bcd3b1ae5e033a6e45470fa2d8d
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-06-17 20:14:37 -0500

    re worked table and the way delete works

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-$(onReady)
+$(document).ready(onReady);
 
 function onReady(){
-    console.log('jq loaded');
-    $('#submit-button').on('click',handleAddClick)
-    $('#delete-button').on('click',handleDeleteClick)
+    $(".add-row").click(handleAddClick);
+    $(".delete-row").click(handleDeleteClick);
+
+function handleAddClick(){
+        let firstName = $("#first-name").val();
+        let lastName = $("#last-name").val();
+        let eID = $("#eID").val();
+        let title = $("#title").val();
+        let salary = $("#salary").val();
+        let employeeInfo = "<tr><td><input type='checkbox' name='record'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + eID + "</td><td>" + title + "</td><td>" + salary + "</td></tr>"
+        $("table tbody").append(employeeInfo);
+};
+
+// Find and remove selected table rows
+function handleDeleteClick(){
+        $("table tbody").find('input[name="record"]').each(function(){
+            if($(this).is(":checked")){
+                $(this).parents("tr").remove();
+            }
+        });
+    };
+};
 
 
-}
 
-function
+// function handleAddClick(){
+//     console.log('in add');
+//     // take the information form the input field
+//     $().append($('#first-name').val())
+//     $('#ln-slot').append($('#last-name').val())
+//     $('#eID-slot').append($('#eID').val())
+//     $('#title-slot').append($('#title').val())
+//     $('#salary-slot').append($('#salary').val())
+// }
+

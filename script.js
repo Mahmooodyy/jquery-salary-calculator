a0ad70845ae7bd99ec3ba436d320988ff6755e6b
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-06-19 14:31:05 -0500

    wrapped up the addition and division functions

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function onReady(){
     clearInputs()
 };
 
-let monthlyCost = 0
+let total = 0
 
 function handleAddClick(){
     let firstName = $("#first-name").val();
@@ -17,16 +17,42 @@ function handleAddClick(){
     let salary = $("#salary").val();
     let employeeInfo = "<tr><td><input type='checkbox' name='selector'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + eID + "</td><td>" + title + "</td><td>" + salary + "</td></tr>"
     $("table tbody").append(employeeInfo);
-    clearInputs()
+    
 
-    $('#monthly-cost').empty().append(monthlyCost);
+    let employees = [];
+
+    const employee = {
+        firstName: $("#first-name").val(),
+        lastName: $("#last-name").val(),
+        eID: $("#eID").val(),
+        title: $("#title").val(),
+        salary: $("#salary").val()
+    }
+
+    employees.push(employee);
+    console.log(employees);
+
+    for (let i = 0; i < employees.length; i++) {
+
+        let first = total += Number(employee.salary)
+        let complete = Number(first) / 12
+        console.log(Number(complete));
+        $('#monthly-cost').empty().append(Number(complete));
+
+        if (complete >= 20000) {
+            console.log('ruh roh');
+            $('#change-red').css('color', 'red');
+        }
+
+    }
+    clearInputs()
 };
 
 function handleDeleteClick(){
     $("table tbody").find('input[name="selector"]').each(function(){
     if($(this).is(":checked")){
     $(this).parents("tr").remove();
-}
+};
 });
 };
 
